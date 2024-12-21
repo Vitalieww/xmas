62d5b17f56664bc90baed3500256aa9de87fe260
[Moisei-D] <[daniel.moisei@isa.utm.md]>
2024-12-21 23:10:55 +0200

    commmit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from typing import Final
 from telegram import Update 
 from telegram.ext import Application, CommandHandler, MessageHandler, ContextTypes, filters
-
+from algocop import text_analysis, api_key
 TOKEN: Final = "8072728393:AAEv6-XZ7e2JRIa0Pt9hTyur6Z3OIef_7uI"
 BOT_USRENAME: Final = "@reality_checkerbot"
 
@@ -14,7 +14,6 @@ async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
         "Just send me the news and I will tell you if it is real or fake.\n\n"
         "Here are some commands you can use:\n"
         "/start - Start the bot and see this message\n"
-        "/help - Get help on how to use the bot\n"
         "You can also just send me any news article or text, and I will analyze it for you."
     )
     await update.message.reply_text(start_message)
@@ -25,6 +24,9 @@ async def save_user_input(update: Update, context: ContextTypes.DEFAULT_TYPE) ->
     print(f"Saved user input: {user_input_variable}")  # Print the saved input for confirmation
     await update.message.reply_text(f"Your input has been saved")  # Await the reply_text coroutine
     await update.message.reply_text(f"Searching for the reality of the news...")  # Await the reply_text coroutine
+    response = text_analysis(user_input_variable, api_key)  # Call the text_analysis function with the user's input
+    await update.message.reply_text(response)  # Await the reply_text coroutine with the response
+    
 
 # Responses
 def handle_response(text: str) -> str:
