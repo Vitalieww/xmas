9362c50fa413fecb00b43aa76943251688941cff
[Moisei-D] <[daniel.moisei@isa.utm.md]>
2024-12-21 18:38:02 +0200

    First Commit

--- main.py
+++ main.py
@@ -27,6 +27,37 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     
     print(f"User ({update.message.chat.id}) in {message_type}: {text}")
     
-    
+    if message_type == "group":
+        if BOT_USRENAME in text:
+            next_text: str = text.replace(BOT_USRENAME, "")
+            response: str = handle_response(next_text)
+        else:
+            return
+    else:
+        response : str = handle_response(text)
+        
+    print("Bot: ", response)
+    await update.message.reply_text(response)
 
+async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print(f"Update {update} caused error {context.error}")
+    
+if __name__ == '__main__':
+    print("Starting bot...")
+    app = Application.builder().token(TOKEN).build()
+    
+    #Commands
+    app.add_handler(CommandHandler("start", start_command))
+    app.add_handler(CommandHandler("help", help_command))
+    app.add_handler(CommandHandler("custom", custom_command))
+
+    #Messages
+    app.add_handler(MessageHandler(filters.TEXT, handle_message))
+    
+    #Error
+    app.add_error_handler(error)
+    
+    #Polling
+    print("Polling...")
+    app.run_polling(poll_interval=3)
 
